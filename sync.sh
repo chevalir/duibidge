2156ca500e0644ca5d5b4d865fa77ec5173ee5cc
chevalir <chevalir01@gmail.com>
2019-03-19 10:38:22 +0100

    Remove other unsused files

--- sync.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cd /var/www/html/plugins/duibridge ; sudo cp -r /home/pi/pidomo/jeedom/duibridge/* .; sudo chown -R www-data:www-data  *
--- togit.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#rsync -r -update /Volumes/RpiHome/pidomo/jeedom/duibridge /Users/chevalir/Dev/Jeedom/git-root/duibridge
-cp -r /Volumes/RpiHome/pidomo/jeedom/duibridge/* /Users/chevalir/Dev/Jeedom/git-root/duibridge/.
